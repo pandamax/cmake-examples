57fb8ed9aaf336473b4e09129318e591e3995dd6
Yinguan <1286425661@qq.com>
2022-01-24 15:20:02 +0800

    Create linux_example.sh

---
+++ 08-cross-compile/gen_testbed_dst/build/linux_example.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BUILD_DIR=linux_example
+rm -rf ${BUILD_DIR}
+mkdir ${BUILD_DIR}
+cd ${BUILD_DIR}
+
+set BUILD_TYPE=Release
+BUILD_TYPE=Release
+mkdir $BUILD_TYPE
+# -DUSE_LOG=ON
+cmake ../../ -DCMAKE_BUILD_TYPE=Release 
+
+make
